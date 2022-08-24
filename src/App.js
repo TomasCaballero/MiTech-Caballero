264ab70d2e8ab1c67cb45cea8abbbe9d85bcea10
Tomas Caballero <caballerotomas97@gmail.com>
2022-08-24 15:44:33 -0300

    navBar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="navBar">
+        <div className="miMarca">
+          <a href="../public/index.html" className="miMarca"><img src={logo} className="logoNavBar" alt="logo" />MiTech</a>
+        </div>
+        <div className="pages">
+          <ul className="ulNavBar">
+            <li>
+              <a href="telefonos.html">Telefonos</a>
+            </li>
+            <li>
+              <a href="computadoras.html">Computadoras</a>
+            </li>
+            <li>
+              <a href="componentes.html">Componentes</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
